282d00ca3460f5e8a5a928550a71bbda9cd76a12
NaaelMufti <naaelm@my.ofs.edu.sg>
2021-09-20 15:21:39 +0800

    Initial commit

---
+++ multiplication.java
@@ -0,0 +1,12 @@
+
+public class multiplication {
+	
+	public static void main (String[] args) 
+	{
+		int a = IBIO.inputInt("enter a number: ");
+		int b = IBIO.inputInt("and another number: ");
+		int c = a * b;
+		IBIO.output ("the product of " + a + " and " + b + " is " + c);
+	}
+}
+
