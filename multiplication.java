101ca2e746abf0cbef136195e11b5fa21f8f8d54
NaaelMufti <naaelm@my.ofs.edu.sg>
2021-09-21 12:48:16 +0800

    Grammar

--- multiplication.java
+++ multiplication.java
@@ -6,7 +6,7 @@ public class multiplication {
 		int a = IBIO.inputInt("enter a number: ");
 		int b = IBIO.inputInt("and another number: ");
 		int c = a * b;
-		IBIO.output ("the multiplication of " + a + " and " + b + " is " + c);
+		IBIO.output ("the product of " + a + " and " + b + " is " + c);
 	}
 }
 
