60148b6c9ca3b2711977850aa2b2c1d8a99e2e89
NaaelMufti <naaelm@my.ofs.edu.sg>
2021-11-10 14:05:22 +0800

    Create formatting.java

---
+++ formatting.java
@@ -0,0 +1,24 @@
+
+
+public class formatting {
+	
+	public static void main (String[] args) 
+	{
+		int x=IBIO.inputInt("Enter a number: ");
+			if (x < 10)
+			{
+				IBIO.out(" ");
+			}
+			if (x < 100)
+			{
+				IBIO.out(" ");
+			}
+			IBIO.output(x);
+	}
+}
+
+		/* outputs spacing so it is alligned right
+		 * for numbers less then 10 it works under both x<100 and x<10 
+		 * if with is assumed, then put that amount of spaces or width - digits
+		 * for centre, padding = (80 - digits)/2
+		 */
