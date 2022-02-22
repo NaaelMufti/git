cfd78a65963e162c5ac286532dfe73534084a0e4
NaaelMufti <88817489+NaaelMufti@users.noreply.github.com>
2022-02-22 22:52:48 +0800

    Update

---
+++ G11Sem1/Chapter6/Worksheet6_2.java
@@ -0,0 +1,26 @@
+
+public class Worksheet6_2 
+{	
+	public static void main (String[] args) 
+	{
+		int lines = IBIO.inputInt("Enter number of lines: ");
+		int i;
+		for (i = 1; i<=lines; i++)
+		{
+			IBIO.output(star(i));
+		}
+		for (i = (lines-1); i>=1; i--)
+		{
+			IBIO.output(star(i));
+		}
+	}
+	static String star(int n)
+	{ 
+		String output = "";
+		for (int i = 1; i<=n; i++)
+		{
+			output = output + "*";
+		}
+		return output;
+	}
+}
