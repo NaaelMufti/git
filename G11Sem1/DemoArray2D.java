2d87ee36636ebf708adcef3962926d9ecd0b53e2
NaaelMufti <naaelm@my.ofs.edu.sg>
2022-03-30 13:58:40 +0800

    Update

---
+++ G11Sem1/DemoArray2D.java
@@ -0,0 +1,22 @@
+
+public class DemoArray2D
+{
+	public static void printArray(String[] a)
+	{
+		for(int i = 0; i < a.length; i++)
+		{
+			System.out.print( "  " + a[i]);
+		}
+	}
+	
+	public static void main (String[] args) 
+	{
+		int [] [] a2d = 
+		{
+			{1, 2, 7, 5},
+			{3, 4, 5, 9},
+			{11, 13, 21, 41}
+		};
+	}
+}
+
